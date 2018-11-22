3d293bf62b6672535bc3b4d9dc85366bae5a144e
wenbin6286 <wenbin6286@gmail.com>
2018-11-22 01:05:18 -0500

    remove sql files

--- src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-insert into customer(name, email,created_data) value('bwen','bwen@localhost',sysdate());
--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists customer;
-create table customer (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(100) not null,
-email VARCHAR(100) not null,
-created_on date not null,
-primary key (id));
